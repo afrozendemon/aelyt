5b6a3e28908859fec738b51b23ad59be0a059eea
A Enany <a.enany91@gmail.com>
2017-08-27 17:18:30 -0400

    a design pattern we can work with

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 
 const {app, BrowserWindow} = require('electron');
 
+var mainplayer = require('./player.js');
+
 let mainWindow;
 
 // Quit when all windows are closed.
@@ -15,6 +17,8 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
+
+  mainplayer.printMessage();
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
