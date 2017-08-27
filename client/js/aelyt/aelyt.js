293cd2683c70da2b5019258c06129a2d6fb1a1d7
A Enany <a.enany91@gmail.com>
2017-08-27 19:55:28 -0400

    add some audio packages.

--- client/js/aelyt/aelyt.js
+++ client/js/aelyt/aelyt.js
@@ -1,5 +1,7 @@
 const {ipcRenderer} = require('electron')
-
+const stream = require('youtube-audio-stream');
+const decoder = require('lame').Decoder;
+const speaker = require('speaker');
 
 document.getElementById("clickButton").addEventListener("click", function(){
     console.log('clicked')
@@ -7,10 +9,23 @@ document.getElementById("clickButton").addEventListener("click", function(){
     
     ipcRenderer.on('asynchronous-reply', (event, arg) => {
       console.log(arg) // prints "pong"
-    })
+    });
+
     ipcRenderer.send('asynchronous-message', 'ping')
 }); 
 
+
+var playButton = $('#playButton');
+
+playButton.on('click', function(){
+  console.log('playing...');
+
+  var url = 'http://youtube.com/watch?v=QzBsdMfmCZU';
+
+  stream(url).pipe(decoder()).pipe(speaker());
+  
+});
+
 $(document).on('ready', function(){
-  console.log('DOM content loaded')
+  console.log('DOM content loaded');
 })
